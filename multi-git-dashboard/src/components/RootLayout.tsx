35b7d69b595c12efeda2acb6f55831a6e97a4e50
auhc99 <auhc99@gmail.com>
2023-08-22 23:53:27 +0800

    Refactor RootLayout

---
+++ multi-git-dashboard/src/components/RootLayout.tsx
@@ -0,0 +1,22 @@
+import React, { ReactNode } from 'react';
+import Sidebar from './Sidebar';
+import styles from '../styles/RootLayout.module.css';
+
+interface RootLayoutProps {
+  children: ReactNode;
+}
+
+class RootLayout extends React.Component<RootLayoutProps> {
+  render() {
+    const { children } = this.props;
+
+    return (
+      <div className={styles.rootLayout}>
+        <Sidebar />
+        <div className={styles.content}>{children}</div>
+      </div>
+    );
+  }
+}
+
+export default RootLayout;
