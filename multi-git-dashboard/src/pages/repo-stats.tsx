3cf414c5f15a10692d69707599397a7ffb652912
Lin Fangyuan <linfangyuan99@gmail.com>
2023-08-24 21:47:49 +0800

    refactor: github stats page

--- multi-git-dashboard/src/pages/repo-stats.tsx
+++ multi-git-dashboard/src/pages/repo-stats.tsx
@@ -1,36 +1,43 @@
 import RepoCard from '@/components/RepoCard';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { githubApp } from './api/github';
 
-const RepoStatsPage: React.FC = async () => {
-  const installations = await githubApp.octokit.request('GET /users/{username}/installation', {
-    username: 'NUS-CRISP',
-    headers: {
-      'X-GitHub-Api-Version': '2022-11-28'
-    }
-  });
+const RepoStatsPage: React.FC = () => {
+  const [weeks, setWeeks] = React.useState<number[][]>([]);
 
-  const INSTALLATION_ID = installations.data.id;
+  useEffect(() => {
+    async function getData() {
+      const installations = await githubApp.octokit.request('GET /users/{username}/installation', {
+        username: 'NUS-CRISP',
+        headers: {
+          'X-GitHub-Api-Version': '2022-11-28'
+        }
+      });
 
-  const octokit = await githubApp.getInstallationOctokit(INSTALLATION_ID);
+      const INSTALLATION_ID = installations.data.id;
 
-  const res = await octokit.request('GET /repos/{owner}/{repo}/stats/code_frequency', {
-    owner: 'NUS-CRISP',
-    repo: 'CRISP',
-    headers: {
-      'X-GitHub-Api-Version': '2022-11-28'
-    }
-  });
+      const octokit = await githubApp.getInstallationOctokit(INSTALLATION_ID);
 
-  const weeks = res.status === 200 ? res.data : [];
+      const res = await octokit.request('GET /repos/{owner}/{repo}/stats/code_frequency', {
+        owner: 'NUS-CRISP',
+        repo: 'CRISP',
+        headers: {
+          'X-GitHub-Api-Version': '2022-11-28'
+        }
+      });
 
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex flex-col items-center justify-center">
-        <RepoCard weeks={weeks} />
-      </div>
-    </main>
-  );
+      if (res.status === 200) {
+        setWeeks(res.data);
+      }
+    }}, []);
+
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <div className="flex flex-col items-center justify-center">
+          <RepoCard weeks={weeks} />
+        </div>
+      </main>
+    );
 }
 
 export default RepoStatsPage;
