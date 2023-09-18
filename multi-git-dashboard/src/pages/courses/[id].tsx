fdf07ea9902a5014d178013736a54171d540518e
auhc99 <auhc99@gmail.com>
2023-09-18 11:53:23 +0800

    Create milestone adding and viewing functionality

--- multi-git-dashboard/src/pages/courses/[id].tsx
+++ multi-git-dashboard/src/pages/courses/[id].tsx
@@ -2,10 +2,11 @@ import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import { Course } from '@/types/course';
 import { Container, Loader, Button, Tabs } from '@mantine/core';
-import StudentForm from '@/components/forms/StudentForm';
 import CourseInfo from '@/components/CourseView/CourseInfo';
 import StudentsInfo from '@/components/CourseView/StudentsInfo';
 import TeamSetsInfo from '@/components/CourseView/TeamSetsInfo';
+import MilestonesInfo from '@/components/CourseView/MilestonesInfo';
+import SprintsInfo from '@/components/CourseView/SprintsInfo';
 
 const backendPort = process.env.BACKEND_PORT || 3001;
 const apiUrl = `http://localhost:${backendPort}/api/courses/`;
@@ -14,8 +15,8 @@ const apiUrl = `http://localhost:${backendPort}/api/courses/`;
 const CourseViewPage: React.FC = () => {
   const router = useRouter();
   const id = router.query.id as string;
-  const [course, setCourse] = useState<Course | null>(null);
-  const [showForm, setShowForm] = useState(false);
+  const [course, setCourse] = useState<Course>();
+  
 
   useEffect(() => {
     if (id) {
@@ -28,6 +29,19 @@ const CourseViewPage: React.FC = () => {
       const response = await fetch(`${apiUrl}${id}`);
       if (response.ok) {
         const data = await response.json();
+        if (data.milestones) {
+          data.milestones = data.milestones.map((milestone : any) => ({
+            ...milestone,
+            dateline: new Date(milestone.dateline),
+          }));
+        }
+        if (data.sprints) {
+          data.sprints = data.sprints.map((sprint : any) => ({
+            ...sprint,
+            startDate: new Date(sprint.startDate),
+            endDate: new Date(sprint.endDate),
+          }));
+        }
         setCourse(data);
       } else {
         console.error('Error fetching course:', response.statusText);
@@ -52,9 +66,8 @@ const CourseViewPage: React.FC = () => {
     }
   };
 
-  const handleStudentCreated = () => {
+  const handleUpdate = () => {
     fetchCourse();
-    setShowForm(false);
   };
 
   if (!course) {
@@ -73,8 +86,8 @@ const CourseViewPage: React.FC = () => {
             <Tabs.Tab value="info">Course Info</Tabs.Tab>
             <Tabs.Tab value="students">Students</Tabs.Tab>
             <Tabs.Tab value="teams">Teams</Tabs.Tab>
-            <Tabs.Tab value="sprints">Sprints</Tabs.Tab>
             <Tabs.Tab value="milestones">Timeline</Tabs.Tab>
+            <Tabs.Tab value="sprints">Sprints</Tabs.Tab>
           </Tabs.List>
           <Tabs.Panel value="info">
             <div>
@@ -88,18 +101,22 @@ const CourseViewPage: React.FC = () => {
           </Tabs.Panel>
           <Tabs.Panel value="students">
             <div>
-              <StudentsInfo students={course.students} />
-              <div>
-                <Button onClick={() => setShowForm(!showForm)} style={{ marginTop: '16px' }}>
-                  {showForm ? 'Cancel' : 'Add Student'}
-                </Button>
-                {showForm && <StudentForm courseId={id} onStudentCreated={handleStudentCreated} />}
-              </div>
+              <StudentsInfo course={course} onUpdate={handleUpdate}/>
             </div>
           </Tabs.Panel>
           <Tabs.Panel value="teams">
             <div>
-              <TeamSetsInfo course={course} />
+              <TeamSetsInfo course={course} onUpdate={handleUpdate}/>
+            </div>
+          </Tabs.Panel>
+          <Tabs.Panel value="milestones">
+            <div>
+              <MilestonesInfo course={course} onUpdate={handleUpdate}/>
+            </div>
+          </Tabs.Panel>
+          <Tabs.Panel value="sprints">
+            <div>
+              <SprintsInfo course={course} onUpdate={handleUpdate}/>
             </div>
           </Tabs.Panel>
         </Tabs>
