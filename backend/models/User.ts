fb862ddc20e4eea61e25df00db68bf333bced7c7
auhc99 <auhc99@gmail.com>
2023-10-18 13:43:22 +0800

    Update User.ts

--- backend/models/User.ts
+++ backend/models/User.ts
@@ -2,14 +2,21 @@ import mongoose, { Schema } from 'mongoose';
 
 export interface User {
   name: string;
+  identifier: string;
   enrolledCourses: mongoose.Types.ObjectId[];
   gitHandle: string;
+  role: string;
 }
 
 export const userSchema = new Schema({
   name: { type: String, required: true },
+  identifier: { type: String, required: true, unique: true},
   enrolledCourses: [{ type: Schema.Types.ObjectId, ref: 'Course' }],
   gitHandle: { type: String },
+  role: {
+    type: String,
+    enum: ['Faculty member', 'Teaching assistant', 'Student'],
+  },
 });
 
 const UserModel = mongoose.model('User', userSchema);
