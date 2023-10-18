578bc6573240b1e7ee2a42bb5982df21d9a211c7
auhc99 <auhc99@gmail.com>
2023-10-18 23:27:21 +0800

    Update TeamCard.tsx

--- multi-git-dashboard/src/components/CourseView/Cards/TeamCard.tsx
+++ multi-git-dashboard/src/components/CourseView/Cards/TeamCard.tsx
@@ -1,14 +1,43 @@
 import React from 'react';
-import { Card, Text, Group, Table } from '@mantine/core';
+import { Card, Text, Group, Table, Button } from '@mantine/core';
 import { User } from '@/types/user';
 
 interface TeamCardProps {
+  teamId: string;
   number: number;
   members: User[];
   TA: User;
+  onTeamDeleted: () => void;
 }
 
-const TeamCard: React.FC<TeamCardProps> = ({ number, members, TA }) => {
+const backendPort = process.env.BACKEND_PORT || 3001;
+
+const TeamCard: React.FC<TeamCardProps> = ({
+  teamId,
+  number,
+  members,
+  TA,
+  onTeamDeleted,
+}) => {
+  const handleDelete = async () => {
+    try {
+      const response = await fetch(
+        `http://${process.env.NEXT_PUBLIC_DOMAIN}:${backendPort}/api/teams/${teamId}`,
+        {
+          method: 'DELETE',
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error('Failed to delete the team');
+      }
+      console.log('Team deleted');
+      onTeamDeleted();
+    } catch (error) {
+      console.error('Error deleting team:', error);
+    }
+  };
+
   const student_rows = members?.map(member => {
     if (member.role === 'Student') {
       return (
@@ -40,6 +69,9 @@ const TeamCard: React.FC<TeamCardProps> = ({ number, members, TA }) => {
         </thead>
         <tbody>{student_rows}</tbody>
       </Table>
+      <Button onClick={handleDelete} style={{ marginBottom: '16px' }}>
+        Delete Team
+      </Button>
     </Card>
   );
 };
