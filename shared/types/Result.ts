794237a19e72330e055e48bff4ed07ecae8224fc
auhc99 <auhc99@gmail.com>
2023-11-19 02:04:42 +0800

    Update Result.ts

--- shared/types/Result.ts
+++ shared/types/Result.ts
@@ -2,10 +2,16 @@ import { Assessment } from './Assessment';
 import { Team } from './Team';
 import { User } from './User';
 
+export interface Mark {
+  userId: string;
+  name: string;
+  mark: number;
+}
+
 export interface Result {
   _id: string;
   assessment: Assessment;
   team: Team;
   marker: User;
-  marks: { userId: string; name: string; mark: number; }[];
+  marks: Mark[];
 }
