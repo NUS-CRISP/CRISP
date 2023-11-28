72afaf7369ff8b903ad453576d122847b698047d
auhc99 <auhc99@gmail.com>
2023-11-28 15:37:58 +0800

    Update StudentForm.tsx

--- multi-git-dashboard/src/components/forms/StudentForm.tsx
+++ multi-git-dashboard/src/components/forms/StudentForm.tsx
@@ -1,18 +1,7 @@
-import {
-  Box,
-  Button,
-  Notification,
-  Group,
-  Text,
-  TextInput,
-} from '@mantine/core';
-import { Dropzone, MIME_TYPES } from '@mantine/dropzone';
+import { Box, Button, Notification, TextInput } from '@mantine/core';
 import { useForm } from '@mantine/form';
-import { User } from '@shared/types/User';
-import { IconPhoto, IconUpload, IconX } from '@tabler/icons-react';
-import { saveAs } from 'file-saver';
-import Papa, { ParseResult } from 'papaparse';
-import { useCallback, useState } from 'react';
+import { useState } from 'react';
+import CSVUpload from './CSVUpload';
 
 interface StudentFormProps {
   courseId: string | string[] | undefined;
@@ -31,93 +20,29 @@ const StudentForm: React.FC<StudentFormProps> = ({
       email: '',
     },
   });
-  const [students, setStudents] = useState<User[]>([]);
   const [error, setError] = useState<string | null>(null);
-
-  const handleFileUpload = useCallback((file: File) => {
-    if (file) {
-      const reader = new FileReader();
-      reader.onload = function () {
-        Papa.parse(reader.result as string, {
-          header: true,
-          skipEmptyLines: true,
-          complete: function (results: ParseResult<User>) {
-            setStudents(results.data);
-          },
-          error: function (error: Error) {
-            console.error('CSV parsing error:', error.message);
-            setError('Error parsing CSV. Please check the format.');
-          },
-        });
-      };
-      reader.readAsText(file);
-    }
-  }, []);
-
-  const downloadCsvTemplate = () => {
-    const csvHeaders = 'name,identifier,email,gitHandle\n';
-    const blob = new Blob([csvHeaders], { type: 'text/csv;charset=utf-8' });
-    saveAs(blob, 'students_template.csv');
-  };
-
-  const handleSubmitCSV = async () => {
-    if (students.length === 0) {
-      console.log('No students to upload.');
-      return;
-    }
-
-    console.log('Sending students data:', students);
-
-    try {
-      const response = await fetch(
-        `http://localhost:${process.env.NEXT_PUBLIC_BACKEND_PORT}/api/courses/${courseId}/students`,
-        {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            items: students,
-          }),
-        }
-      );
-
-      if (response.ok) {
-        const data = await response.json();
-        console.log('Students created:', data);
-        onStudentCreated();
-      } else {
-        console.error('Error uploading students:', response.statusText);
-        setError('Error uploading students. Please try again.');
-      }
-    } catch (error) {
-      console.error('Error uploading students:', error);
-      setError('Error uploading students. Please try again.');
-    }
-  };
+  const apiUrl = `http://localhost:${process.env.NEXT_PUBLIC_BACKEND_PORT}/api/courses/${courseId}/students`;
+  const csvTemplateHeaders = 'name,identifier,email,gitHandle';
 
   const handleSubmitForm = async () => {
     console.log('Sending student data:', form.values);
 
-    const response = await fetch(
-      `http://localhost:${process.env.NEXT_PUBLIC_BACKEND_PORT}/api/courses/${courseId}/students`,
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          items: [
-            {
-              identifier: form.values.identifier,
-              name: form.values.name,
-              gitHandle: form.values.gitHandle,
-              email: form.values.email,
-            },
-          ],
-        }),
-      }
-    );
+    const response = await fetch(apiUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        items: [
+          {
+            identifier: form.values.identifier,
+            name: form.values.name,
+            gitHandle: form.values.gitHandle,
+            email: form.values.email,
+          },
+        ],
+      }),
+    });
 
     const data = await response.json();
     console.log('Student created:', data);
@@ -171,56 +96,14 @@ const StudentForm: React.FC<StudentFormProps> = ({
           Create Student
         </Button>
       </form>
-
-      <Dropzone
-        onDrop={(files: File[]) => {
-          console.error(files);
-          if (files.length > 0) {
-            handleFileUpload(files[0]);
-          }
-        }}
-        accept={[MIME_TYPES.csv]}
-        maxSize={1024 * 1024 * 5}
-        maxFiles={1}
-        multiple={false}
-        style={{ marginTop: '16px' }}
-      >
-        <Group mih={220} style={{ pointerEvents: 'none' }}>
-          <Dropzone.Accept>
-            <IconUpload
-              style={{ color: 'var(--mantine-color-blue-6)' }}
-              stroke={1.5}
-            />
-          </Dropzone.Accept>
-          <Dropzone.Reject>
-            <IconX
-              style={{ color: 'var(--mantine-color-red-6)' }}
-              stroke={1.5}
-            />
-          </Dropzone.Reject>
-          <Dropzone.Idle>
-            <IconPhoto
-              style={{ color: 'var(--mantine-color-dimmed)' }}
-              stroke={1.5}
-            />
-          </Dropzone.Idle>
-
-          <div>
-            <Text size="xl" inline>
-              Drag images here or click to select files
-            </Text>
-            <Text size="sm" c="dimmed" inline mt={7}>
-              Attach as many files as you like, each file should not exceed 5mb
-            </Text>
-          </div>
-        </Group>
-      </Dropzone>
-      <Button onClick={handleSubmitCSV} style={{ marginTop: '16px' }}>
-        Upload Students
-      </Button>
-      <Button onClick={downloadCsvTemplate} style={{ marginTop: '16px' }}>
-        Download CSV Template
-      </Button>
+      <CSVUpload
+        templateHeaders={csvTemplateHeaders}
+        onProcessComplete={onStudentCreated}
+        onError={setError}
+        downloadFilename="students_template.csv"
+        uploadButtonString="Upload Students"
+        urlString={apiUrl}
+      />
     </Box>
   );
 };
