467be8d29f63b82f56cb8a83f17cb4544b0e65d2
auhc99 <auhc99@gmail.com>
2023-11-29 17:11:14 +0800

    Create Team Service

--- backend/controllers/teamController.ts
+++ backend/controllers/teamController.ts
@@ -1,26 +1,10 @@
 import { Request, Response } from 'express';
-import Team from '../models/Team';
-import TeamSet from '../models/TeamSet';
+import { deleteTeamById, updateTeamById } from '../services/teamService';
 
 export const deleteTeam = async (req: Request, res: Response) => {
   const teamId = req.params.id;
-
   try {
-    const team = await Team.findById(teamId);
-    if (!team) {
-      return res.status(404).json({ message: 'Team not found' });
-    }
-    const teamSet = await TeamSet.findById(team.teamSet);
-    if (teamSet && teamSet.teams) {
-      const index = teamSet.teams.indexOf(team._id);
-      if (index !== -1) {
-        teamSet.teams.splice(index, 1);
-      }
-      await teamSet.save();
-    }
-
-    await Team.findByIdAndDelete(teamId);
-
+    await deleteTeamById(teamId);
     return res.status(200).json({ message: 'Team deleted successfully' });
   } catch (error) {
     res.status(400).json({ error: 'Failed to delete the team' });
@@ -30,14 +14,8 @@ export const deleteTeam = async (req: Request, res: Response) => {
 export const updateTeam = async (req: Request, res: Response) => {
   const teamId = req.params.id;
   try {
-    const updatedTeam = await Team.findByIdAndUpdate(teamId, req.body, {
-      new: true,
-    });
-    if (updatedTeam) {
-      res.json(updatedTeam);
-    } else {
-      res.status(404).json({ error: 'Team not found' });
-    }
+    const updatedTeam = await updateTeamById(teamId, req.body);
+    res.json(updatedTeam);
   } catch (error) {
     res.status(400).json({ error: 'Failed to update team' });
   }
---
+++ backend/services/teamService.ts
@@ -0,0 +1,27 @@
+// teamService.ts
+import Team from '../models/Team';
+import TeamSet from '../models/TeamSet';
+
+export const deleteTeamById = async (teamId: string) => {
+  const team = await Team.findById(teamId);
+  if (!team) {
+    throw new Error('Team not found');
+  }
+  const teamSet = await TeamSet.findById(team.teamSet);
+  if (teamSet && teamSet.teams) {
+    const index = teamSet.teams.indexOf(team._id);
+    if (index !== -1) {
+      teamSet.teams.splice(index, 1);
+    }
+    await teamSet.save();
+  }
+  await Team.findByIdAndDelete(teamId);
+};
+
+export const updateTeamById = async (teamId: string, updateData: any) => {
+  const updatedTeam = await Team.findByIdAndUpdate(teamId, updateData, { new: true });
+  if (!updatedTeam) {
+    throw new Error('Team not found');
+  }
+  return updatedTeam;
+};
