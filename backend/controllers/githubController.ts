fdd5b41fed596059db6d8b79a2295e6b8fba03b4
auhc99 <auhc99@gmail.com>
2023-11-29 22:07:54 +0800

    Create Github Service

--- backend/controllers/githubController.ts
+++ backend/controllers/githubController.ts
@@ -1,56 +1,51 @@
 import { Request, Response } from 'express';
-import { RequestError } from 'octokit';
-import TeamData from '../models/TeamData';
-import { getGitHubApp } from '../utils/github';
+import {
+  checkGitHubInstallation,
+  fetchAllTeamData,
+  fetchAllTeamDataForOrg,
+} from '../services/githubService';
+import { NotFoundError } from '../services/errors';
 
 export const getAllTeamData = async (req: Request, res: Response) => {
   try {
-    const teamData = await TeamData.find({});
+    const teamData = await fetchAllTeamData();
     return res.status(200).json({ teamData });
   } catch (error) {
-    return res.status(400).json({ error: 'Failed to get all teams data' });
+    console.error('Error retrieving all teams data:', error);
+    return res.status(500).json({ error: 'Failed to get all teams data' });
   }
 };
 
 export const getAllTeamDataForOrg = async (req: Request, res: Response) => {
   try {
-    const teamDatas = await TeamData.find({
-      gitHubOrgName: req.params.gitHubOrgName.toLowerCase(),
-    });
-    if (!teamDatas) {
-      return res.status(404).json({ error: 'Team datas not found' });
-    }
+    const teamDatas = await fetchAllTeamDataForOrg(req.params.gitHubOrgName);
     return res.status(200).json({ teamDatas });
   } catch (error) {
-    return res.status(400).json({ error: 'Failed to get team datas for org' });
+    if (error instanceof NotFoundError) {
+      res.status(404).json({ message: error.message });
+    } else {
+      console.error('Error retrieving team datas for org:', error);
+      return res
+        .status(500)
+        .json({ error: 'Failed to get team datas for org' });
+    }
   }
 };
 
 export const checkInstallation = async (req: Request, res: Response) => {
   // get request body; json with one field containing org name
   const { orgName } = req.body;
-
-  // get github app
-  const octokit = getGitHubApp().octokit;
-
   try {
-    // This will throw an error if the app is not installed on the organization
-    const response = await octokit.rest.apps.getOrgInstallation({
-      org: orgName,
-    });
-    const installationId = response.data.id;
+    const installationId = await checkGitHubInstallation(orgName);
     res.status(200).json({ installationId });
   } catch (error) {
-    if (error instanceof RequestError && error.status === 404) {
-      res.status(404).json({
-        message:
-          'The GitHub App is not installed on the specified organization.',
-      });
+    if (error instanceof NotFoundError) {
+      res.status(404).json({ message: error.message });
     } else {
+      console.error('Error checking github installation:', error);
       res.status(500).json({
         message: 'An error occurred while checking the installation status.',
       });
     }
-    return false;
   }
 };
---
+++ backend/services/githubService.ts
@@ -0,0 +1,39 @@
+import { RequestError } from 'octokit';
+import { getGitHubApp } from '../utils/github';
+import { NotFoundError } from './errors';
+import TeamData from '../models/TeamData';
+
+export const fetchAllTeamData = async () => {
+  return await TeamData.find({});
+};
+
+export const fetchAllTeamDataForOrg = async (gitHubOrgName: string) => {
+  const teamDatas = await TeamData.find({
+    gitHubOrgName: gitHubOrgName.toLowerCase(),
+  });
+  if (!teamDatas) {
+    throw new NotFoundError('Team datas not found');
+  }
+  return teamDatas;
+};
+
+export const checkGitHubInstallation = async (orgName: string) => {
+  const octokit = getGitHubApp().octokit;
+  try {
+    const response = await octokit.rest.apps.getOrgInstallation({
+      org: orgName,
+    });
+    return response.data.id;
+  } catch (error) {
+    if (error instanceof RequestError && error.status === 404) {
+      throw new NotFoundError(
+        'The GitHub App is not installed on the specified organization.'
+      );
+    } else if (error instanceof Error) {
+      throw new Error(error.message);
+    } else {
+      console.error('Unknown Eerror checking github installation:', error);
+      throw new Error('Unknown error checking GitHub installation.');
+    }
+  }
+};
