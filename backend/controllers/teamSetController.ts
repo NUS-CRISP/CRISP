0841293148ac27086c5146fef153c57cc1349b46
auhc99 <auhc99@gmail.com>
2023-11-29 21:16:32 +0800

    Add TeamSet Service

--- backend/controllers/teamSetController.ts
+++ backend/controllers/teamSetController.ts
@@ -1,33 +1,13 @@
 import { Request, Response } from 'express';
-import Course from '../models/Course';
-import Team from '../models/Team';
-import TeamSet from '../models/TeamSet';
+import { deleteTeamSetById } from '../services/teamSetService';
 
 export const deleteTeamSet = async (req: Request, res: Response) => {
   const teamSetId = req.params.id;
-
   try {
-    const teamSet = await TeamSet.findById(teamSetId);
-
-    if (!teamSet) {
-      return res.status(404).json({ message: 'TeamSet not found' });
-    }
-
-    await Team.deleteMany({ teamSet: teamSetId });
-
-    const course = await Course.findById(teamSet.course);
-    if (course) {
-      const index = course.teamSets.indexOf(teamSet._id);
-      if (index !== -1) {
-        course.teamSets.splice(index, 1);
-        await course.save();
-      }
-    }
-
-    await TeamSet.findByIdAndDelete(teamSetId);
-
+    await deleteTeamSetById(teamSetId);
     return res.status(200).json({ message: 'TeamSet deleted successfully' });
   } catch (error) {
-    res.status(400).json({ error: 'Failed to delete the TeamSet' });
+    console.error('Error deleting TeamSet:', error);
+    res.status(400).json({ error: 'Failed to delete TeamSet' });
   }
 };
---
+++ backend/services/teamSetService.ts
@@ -0,0 +1,21 @@
+// teamSetService.ts
+import Course from '../models/Course';
+import Team from '../models/Team';
+import TeamSet from '../models/TeamSet';
+
+export const deleteTeamSetById = async (teamSetId: string) => {
+  const teamSet = await TeamSet.findById(teamSetId);
+  if (!teamSet) {
+    throw new Error('TeamSet not found');
+  }
+  await Team.deleteMany({ teamSet: teamSetId });
+  const course = await Course.findById(teamSet.course);
+  if (course && course.teamSets) {
+    const index = course.teamSets.indexOf(teamSet._id);
+    if (index !== -1) {
+      course.teamSets.splice(index, 1);
+      await course.save();
+    }
+  }
+  await TeamSet.findByIdAndDelete(teamSetId);
+};
