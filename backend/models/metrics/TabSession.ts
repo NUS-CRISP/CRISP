5d9ea705cc7c8b8f1454ff214a15b86e563d2fe0
auhc99 <auhc99@gmail.com>
2024-02-28 21:11:39 +0800

    Set up tab session model

---
+++ backend/models/metrics/TabSession.ts
@@ -0,0 +1,44 @@
+import { TabSession as SharedTabSession } from '@shared/types/metrics/TabSession';
+import mongoose, { Schema, Types } from 'mongoose';
+
+export interface TabSession
+  extends Omit<SharedTabSession, 'account' | 'course'> {
+  account: Types.ObjectId;
+  course: Types.ObjectId;
+}
+
+const TabSessionSchema = new Schema(
+  {
+    account: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Account',
+      required: true,
+    },
+    course: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Course',
+      required: true,
+    },
+    tab: {
+      type: String,
+      required: true,
+    },
+    sessionStartTime: {
+      type: Date,
+      required: true,
+    },
+    sessionEndTime: {
+      type: Date,
+      required: true,
+    },
+    sessionDuration: {
+      type: Number,
+      required: true,
+    },
+  },
+  {}
+);
+
+const TabSessionModel = mongoose.model('TabSession', TabSessionSchema);
+
+export default TabSessionModel;
