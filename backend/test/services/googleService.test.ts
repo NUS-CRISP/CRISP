6c34db694e6f8cd2080ef7f2da3d7c8e88b8545f
auhc99 <auhc99@gmail.com>
2024-03-31 18:54:53 +0800

    Create googleService.test.ts

---
+++ backend/test/services/googleService.test.ts
@@ -0,0 +1,168 @@
+import mongoose from 'mongoose';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import {
+  getAssessmentSheetData,
+  fetchAndSaveSheetData,
+} from '../../services/googleService';
+import SheetDataModel from '../../models/SheetData';
+import AssessmentModel from '../../models/Assessment';
+import AccountModel from '../../models/Account';
+import { NotFoundError } from '../../services/errors';
+import CourseModel from '@models/Course';
+
+jest.mock('../../utils/google');
+
+let mongo: MongoMemoryServer;
+
+beforeAll(async () => {
+  mongo = await MongoMemoryServer.create();
+  const mongoUri = mongo.getUri();
+  await mongoose.connect(mongoUri);
+});
+
+beforeEach(async () => {
+  const collections = await mongoose.connection.db.collections();
+  for (const collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+afterAll(async () => {
+  if (mongo) {
+    await mongo.stop();
+  }
+  await mongoose.connection.close();
+});
+
+afterEach(async () => {
+  jest.resetAllMocks();
+});
+
+describe('googleService', () => {
+  let assessmentId: string;
+
+  beforeEach(async () => {
+    const course = new CourseModel({
+      name: 'Introduction to Computer Science',
+      code: 'CS101',
+      semester: 'Fall 2024',
+      courseType: 'Normal',
+    });
+    await course.save();
+
+    const assessment = new AssessmentModel({
+      course: course._id,
+      assessmentType: 'Exam',
+      markType: 'Percentage',
+      results: [],
+      frequency: 'Once',
+      granularity: 'individual',
+    });
+    await assessment.save();
+    assessmentId = assessment._id.toHexString();
+    course.assessments.push(assessment._id);
+    await course.save();
+
+    const sheetData = new SheetDataModel({
+      headers: ['Student ID', 'Name', 'Grade'],
+      rows: [
+        ['1123', 'hello', '3'],
+        ['2222', 'world', '4'],
+      ],
+      fetchedAt: new Date(),
+    });
+    await sheetData.save();
+    assessment.sheetData = sheetData._id;
+    await assessment.save();
+  });
+
+  describe('getAssessmentSheetData', () => {
+    it('should retrieve sheet data for an assessment', async () => {
+      const account = new AccountModel({
+        email: 'ta1@example.com',
+        password: 'hashedpassword',
+        role: 'Teaching assistant',
+        isApproved: true,
+      });
+      await account.save();
+      const sheetData = await getAssessmentSheetData(assessmentId, account._id);
+
+      expect(sheetData).toBeDefined();
+    });
+
+    it('should throw error for invalid account', async () => {
+      expect(
+        getAssessmentSheetData(
+          assessmentId,
+          new mongoose.Types.ObjectId().toHexString()
+        )
+      ).rejects.toThrow(NotFoundError);
+    });
+
+    it('should throw error for invalid assessment', async () => {
+      const account = new AccountModel({
+        email: 'ta1@example.com',
+        password: 'hashedpassword',
+        role: 'Teaching assistant',
+        isApproved: true,
+      });
+      await account.save();
+      expect(
+        getAssessmentSheetData(
+          new mongoose.Types.ObjectId().toHexString(),
+          account._id
+        )
+      ).rejects.toThrow(NotFoundError);
+    });
+
+    it('should throw error for invalid sheetdata', async () => {
+      const account = new AccountModel({
+        email: 'ta1@example.com',
+        password: 'hashedpassword',
+        role: 'Teaching assistant',
+        isApproved: true,
+      });
+      await account.save();
+      const assessment = await AssessmentModel.findById(assessmentId);
+      if (!assessment) {
+        throw new NotFoundError('Assessment not found');
+      }
+      assessment.sheetData = new mongoose.Types.ObjectId();
+      await assessment.save();
+      expect(getAssessmentSheetData(assessmentId, account._id)).rejects.toThrow(
+        NotFoundError
+      );
+    });
+  });
+
+  describe('fetchAndSaveSheetData', () => {
+    it('fetches and saves new sheet data for an assessment', async () => {
+      const rows = [
+        ['1123', 'hello', '3'],
+        ['2222', 'world', '4'],
+        ['3333', 'next', '5'],
+        ['4444', 'please', '6'],
+      ];
+      const mockFetchDataFromSheet = jest.fn().mockResolvedValue([
+        ['Student ID', 'Name', 'Grade'],
+        ['1123', 'hello', '3'],
+        ['2222', 'world', '4'],
+        ['3333', 'next', '5'],
+        ['4444', 'please', '6'],
+      ]);
+      jest
+        .spyOn(require('../../utils/google'), 'fetchDataFromSheet')
+        .mockImplementation(mockFetchDataFromSheet);
+
+      await fetchAndSaveSheetData(assessmentId, true);
+      const assessment = await AssessmentModel.findById(assessmentId);
+      if (!assessment) {
+        throw new NotFoundError('Assessment not found');
+      }
+      const newSheetData = await SheetDataModel.findById(assessment.sheetData);
+      expect(newSheetData).toBeDefined();
+      expect(newSheetData?.rows).toEqual(rows);
+      expect(newSheetData?.headers).toEqual(['Student ID', 'Name', 'Grade']);
+    });
+  });
+});
