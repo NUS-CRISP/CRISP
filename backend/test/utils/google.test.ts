934d68dbecba885ecc1f95c86f0896c15b704149
auhc99 <auhc99@gmail.com>
2024-03-31 21:05:57 +0800

    Create google.test.ts

---
+++ backend/test/utils/google.test.ts
@@ -0,0 +1,62 @@
+import { fetchDataFromSheet } from '../../utils/google';
+
+const mockSheetId = 'mockSheetId';
+const mockRange = 'Form Responses 1';
+const transformedDataInd = [
+  ['Identifier', 'Name', 'Team', 'Comments'],
+  ['Row1Cell1', 'ROW1CELL2', '1', 'Row1Cell4'],
+  ['Row2Cell1', 'ROW2CELL2', '2', 'Row2Cell4'],
+];
+const transformedDataTeam = [
+  ['Team', 'Comments'],
+  ['1', 'Row1Cell4'],
+  ['2', 'Row2Cell4'],
+];
+
+process.env.GOOGLE_CLIENT_EMAIL = 'mockEmail';
+process.env.GOOGLE_PRIVATE_KEY = 'mockKey';
+
+jest.mock('googleapis', () => {
+  return {
+    google: {
+      auth: {
+        JWT: jest.fn().mockImplementation(() => ({
+          authorize: jest.fn().mockResolvedValue({}),
+        })),
+      },
+      sheets: jest.fn().mockReturnValue({
+        spreadsheets: {
+          values: {
+            get: jest.fn().mockResolvedValue({
+              data: {
+                values: [
+                  ['Identifier', 'Name', 'Team', 'Comments', 'Others'],
+                  ['Row1Cell1', 'Row1Cell2', '1', 'Row1Cell4', 'Row1Cell5'],
+                  ['Row2Cell1', 'Row2Cell2', '2', 'Row2Cell4', 'Row2Cell5'],
+                ],
+              },
+            }),
+          },
+        },
+      }),
+    },
+  };
+});
+
+describe('Google Utils', () => {
+  describe('fetchDataFromSheet', () => {
+    it('fetches data from a sheet and transforms it (individual)', async () => {
+      const data = await fetchDataFromSheet(mockSheetId, mockRange, false);
+      expect(data).toEqual(transformedDataInd);
+    });
+
+    it('fetches data from a sheet and transforms it (team)', async () => {
+      const data = await fetchDataFromSheet(mockSheetId, mockRange, true);
+      expect(data).toEqual(transformedDataTeam);
+    });
+  });
+});
+
+afterAll(() => {
+  jest.restoreAllMocks();
+});
