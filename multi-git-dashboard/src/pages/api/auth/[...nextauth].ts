e25383ab0b94a0275ed03abb1d135638362a6b89
Dexter <dextersimch@gmail.com>
2024-07-20 18:01:16 +0800

    Revert "Fix formatting for prettier"
    This reverts commit 92a7b96df0b6306dc9bdb430a8047bc1bc034d73.

--- multi-git-dashboard/src/pages/api/auth/[...nextauth].ts
+++ multi-git-dashboard/src/pages/api/auth/[...nextauth].ts
@@ -114,10 +114,7 @@ export const getServerSessionHelper = (
 
 export default async function (req: NextApiRequest, res: NextApiResponse) {
   res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate');
-  res.setHeader(
-    'Strict-Transport-Security',
-    'max-age=31536000; includeSubDomains'
-  );
+  res.setHeader('Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
   res.setHeader('X-Content-Type-Options', 'nosniff');
   res.setHeader('X-Frame-Options', 'SAMEORIGIN');
   return NextAuth(req, res, authOptions);
