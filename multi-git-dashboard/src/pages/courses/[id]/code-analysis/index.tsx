8d21e75cc3b18c3470fac7908fd6662494185d61
Rui Jie <80191727+rjkoh@users.noreply.github.com>
2024-10-09 16:00:39 +0800

    remove code analysis frontend redundancies

--- multi-git-dashboard/src/pages/courses/[id]/code-analysis/index.tsx
+++ multi-git-dashboard/src/pages/courses/[id]/code-analysis/index.tsx
@@ -1,15 +1,10 @@
 import CodeAnalysis from '@/components/views/CodeAnalysis';
-import { hasFacultyPermission } from '@/lib/auth/utils';
 import { Container } from '@mantine/core';
 import { useRouter } from 'next/router';
 
 const CodeAnalysisPage: React.FC = () => {
   const router = useRouter();
-  const { pathname, query } = router;
-  const { id } = router.query as {
-    id: string;
-  };
-  const permission = hasFacultyPermission();
+  const { query } = router;
   const courseId = query.id as string;
 
   return (
