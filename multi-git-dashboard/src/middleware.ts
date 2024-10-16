091ebdacd311fb2aa38b247322ed6fb6567247ac
Dexter <dextersimch@gmail.com>
2024-10-17 02:27:06 +0800

    Fix account registration bug with redirection

--- multi-git-dashboard/src/middleware.ts
+++ multi-git-dashboard/src/middleware.ts
@@ -7,8 +7,10 @@ export default withAuth({
 });
 
 // Exclude home ('/') and auth ('/auth/*')
-export const EXCLUDE_AUTH_REGEX = /^(?!\/auth).+/;
+export const EXCLUDE_AUTH_REGEX = /^(?!\/auth|\/api\/accounts\/register).+/;
 
 export const config = {
-  matcher: ['/((?!_next/static|_next/image|auth|favicon.png|$).*)'],
+  matcher: [
+    '/((?!_next/static|_next/image|auth|favicon.png|api/accounts/register|$).*)',
+  ],
 };
