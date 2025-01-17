3786c65b8bf779e88616caa8a05d52ffbf90c310
Rui Jie <80191727+rjkoh@users.noreply.github.com>
2025-01-17 16:32:28 +0800

    add mean and median in tooltip for timeline

--- multi-git-dashboard/src/components/code-analysis/CodeAnalysisTimeline.tsx
+++ multi-git-dashboard/src/components/code-analysis/CodeAnalysisTimeline.tsx
@@ -9,7 +9,9 @@ import {
   Tooltip,
   Legend,
   ResponsiveContainer,
+  TooltipProps,
 } from 'recharts';
+import styles from '../../styles/CodeAnalysisTimeline.module.css';
 
 interface CodeAnalysisTimelineProps {
   codeData: {
@@ -22,6 +24,50 @@ interface CodeAnalysisTimelineProps {
     };
   };
 }
+
+const CustomToolTip: React.FC<TooltipProps<number, string>> = ({
+  active,
+  payload,
+}) => {
+  if (active && payload && payload.length) {
+    const data = payload[0].payload;
+    return (
+      <div className={styles['tooltip-container']}>
+        <p className={styles['tooltip-date']}>
+          <strong>{`Execution date & time: ${new Date(data.executionDate).toLocaleString()}`}</strong>
+        </p>
+
+        {Object.keys(data).map(key => {
+          if (key === 'executionDate') return null; // Skip executionDate here
+          return (
+            <div className={styles['tooltip-metric']} key={key}>
+              <p className={styles['tooltip-metric-title']}>
+                <strong>{key}</strong>
+              </p>
+              <p className={styles['tooltip-metric-value']}>
+                <strong
+                  style={{ color: 'white' }}
+                >{`Value: ${data[key].value}`}</strong>
+              </p>
+              <p className={styles['tooltip-metric-value']}>
+                {`Median: ${
+                  data[key].median === -1 ? 'Not available' : data[key].median
+                }`}
+              </p>
+              <p className={styles['tooltip-metric-value']}>
+                {`Mean: ${
+                  data[key].mean === -1 ? 'Not available' : data[key].mean
+                }`}
+              </p>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+
+  return null;
+};
 
 const CodeAnalysisTimeline: React.FC<CodeAnalysisTimelineProps> = ({
   codeData,
@@ -36,9 +82,18 @@ const CodeAnalysisTimeline: React.FC<CodeAnalysisTimelineProps> = ({
     'Coverage',
   ];
   // Map of domain -> date -> metric -> value
-  const data = new Map<string, Map<Date, Map<string, number>>>();
+  const data = new Map<
+    string,
+    Map<Date, Map<string, { value: number; mean?: number; median?: number }>>
+  >();
   for (const d of domains) {
-    data.set(d, new Map<Date, Map<string, number>>());
+    data.set(
+      d,
+      new Map<
+        Date,
+        Map<string, { value: number; mean?: number; median?: number }>
+      >()
+    );
   }
 
   const [domain, setDomain] = useState<string>('Complexity');
@@ -52,26 +107,46 @@ const CodeAnalysisTimeline: React.FC<CodeAnalysisTimelineProps> = ({
       const metric = dataPoint.metrics[i];
       const value = dataPoint.values[i];
       const domain = dataPoint.domains[i];
+      const metricStatsMap = new Map(Object.entries(dataPoint.metricStats));
 
       // Ignore metrics that are not of the above domains for timeline.
       if (!data.has(domain)) continue;
 
       if (!data.get(domain)?.has(execDate)) {
-        data.get(domain)?.set(execDate, new Map<string, number>());
+        data
+          .get(domain)
+          ?.set(
+            execDate,
+            new Map<string, { value: number; mean?: number; median?: number }>()
+          );
       }
 
-      data.get(domain)?.get(execDate)?.set(metric, parseFloat(value));
+      data
+        .get(domain)
+        ?.get(execDate)
+        ?.set(metric, {
+          value: parseFloat(value),
+          mean: metricStatsMap.get(metric)?.mean || -1,
+          median: metricStatsMap.get(metric)?.median || -1,
+        });
     }
   });
 
-  const chartData = new Map<string, { [key: string]: number | Date }[]>();
+  const chartData = new Map<
+    string,
+    {
+      [key: string]: { value: number; mean?: number; median?: number } | Date;
+    }[]
+  >();
   for (const d of domains) {
     chartData.set(d, []);
   }
 
   for (const [dm, dateMap] of data) {
     for (const [date, metricMap] of dateMap) {
-      const datePoint: { [key: string]: number | Date } = {
+      const datePoint: {
+        [key: string]: { value: number; mean?: number; median?: number } | Date;
+      } = {
         executionDate: date,
       };
       for (const [metric, value] of metricMap) {
@@ -83,9 +158,10 @@ const CodeAnalysisTimeline: React.FC<CodeAnalysisTimelineProps> = ({
   }
 
   const domainData = chartData.get(domain) || [];
-  const xTicks = domainData.map(item =>
-    new Date(item.executionDate).toLocaleDateString()
-  );
+  const xTicks = domainData.map(item => {
+    const executionDate = item.executionDate as Date;
+    return executionDate.toLocaleDateString();
+  });
 
   const handleMouseEnterMetric = (metric: string) => {
     setHoveredMetric(metric);
@@ -119,7 +195,7 @@ const CodeAnalysisTimeline: React.FC<CodeAnalysisTimelineProps> = ({
             tickFormatter={date => new Date(date).toLocaleDateString()}
           />
           <YAxis />
-          <Tooltip />
+          <Tooltip content={<CustomToolTip />} />
           <Legend
             onMouseEnter={({ value }) => handleMouseEnterMetric(value)}
             onMouseLeave={handleMouseLeaveMetric}
@@ -130,7 +206,8 @@ const CodeAnalysisTimeline: React.FC<CodeAnalysisTimelineProps> = ({
               <Line
                 key={metric}
                 type="monotone"
-                dataKey={metric}
+                dataKey={`${metric}.value`}
+                name={metric}
                 stroke={`#${Math.floor(Math.random() * 16777215).toString(16)}`}
               />
             ))}
@@ -138,17 +215,7 @@ const CodeAnalysisTimeline: React.FC<CodeAnalysisTimelineProps> = ({
       </ResponsiveContainer>
 
       {hoveredMetric && metricExplanations[hoveredMetric] && (
-        <div
-          style={{
-            position: 'absolute',
-            top: '20px',
-            left: '20px',
-            padding: '10px',
-            backgroundColor: 'rgba(0, 0, 0, 0.7)',
-            color: 'white',
-            borderRadius: '5px',
-          }}
-        >
+        <div className={styles['hovered-metric-explanation']}>
           <strong>{hoveredMetric}</strong>: {metricExplanations[hoveredMetric]}
         </div>
       )}
