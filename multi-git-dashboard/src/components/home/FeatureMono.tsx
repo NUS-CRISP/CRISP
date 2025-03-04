909085457eb9a0fd340b9a6b5c42563ed32eab51
wxy1203 <e0817822@u.nus.edu>
2025-03-04 11:22:49 +0800

    Update FeatureMono.tsx

--- multi-git-dashboard/src/components/home/FeatureMono.tsx
+++ multi-git-dashboard/src/components/home/FeatureMono.tsx
@@ -83,7 +83,10 @@ const FeatureMono: React.FC = () => {
     const features = MOCKDATA.map((feature, index) => <Feature {...feature} key={index} />);
 
     return (
-        <Container className={classes.wrapper}>
+        <Container className={classes.wrapper}
+        style={{
+            backgroundColor: 'rgba(0, 0, 0)',
+        }}>
             <Title className={classes.title}>Integrate effortlessly with any technology stack</Title>
 
             <Container size={560} p={0}>
@@ -95,14 +98,14 @@ const FeatureMono: React.FC = () => {
                 </Text>
             </Container>
 
-            <SimpleGrid
+            {/* <SimpleGrid
                 mt={60}
                 cols={{ base: 1, sm: 2, md: 3 }}
                 spacing={{ base: 'xl', md: 50 }}
                 verticalSpacing={{ base: 'xl', md: 50 }}
             >
                 {features}
-            </SimpleGrid>
+            </SimpleGrid> */}
         </Container>
     );
 }
