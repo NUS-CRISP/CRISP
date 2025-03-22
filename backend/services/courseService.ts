59d4ef7ec2197aee3c3f4605f3b6088e46ed2858
Kevin Tjan <kevintjan27@gmail.com>
2025-03-23 04:06:08 +0800

    Fix Formatting

--- backend/services/courseService.ts
+++ backend/services/courseService.ts
@@ -181,7 +181,7 @@ export const addStudentsToCourse = async (
         r => r.course === courseId
       );
       if (
-        ((courseRoleTuple.length !== 0) &&
+        (courseRoleTuple.length !== 0 &&
           studentAccount.crispRole !== CrispRole.TrialUser) ||
         studentData.name.toUpperCase() !== student.name.toUpperCase() ||
         studentData.email.toLowerCase() !== studentAccount.email.toLowerCase() // Check is case-insensitive to handle email case-insensitivity cases
@@ -302,7 +302,7 @@ export const addTAsToCourse = async (courseId: string, TADataList: any[]) => {
         r => r.course === courseId
       );
       if (
-        ((courseRoleTuple.length !== 0) &&
+        (courseRoleTuple.length !== 0 &&
           TAAccount.crispRole !== CrispRole.TrialUser) ||
         TAData.name.toUpperCase() !== TA.name.toUpperCase() ||
         TAData.email.toLowerCase() !== TAAccount.email.toLowerCase()
@@ -435,7 +435,7 @@ export const addFacultyToCourse = async (
         r => r.course === courseId
       );
       if (
-        ((courseRoleTuple.length !== 0) &&
+        (courseRoleTuple.length !== 0 &&
           facultyAccount.crispRole !== CrispRole.TrialUser) ||
         facultyData.name.toUpperCase() !== facultyMember.name.toUpperCase() ||
         facultyData.email.toLowerCase() !== facultyAccount.email.toLowerCase()
