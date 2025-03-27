03f9785a1ee0cb04512663190ceeda67a1ee81ca
wxy1203 <e0817822@u.nus.edu>
2025-03-27 13:36:15 +0800

    fix: trial user data

--- multi-git-dashboard/src/components/cards/OverviewCard.tsx
+++ multi-git-dashboard/src/components/cards/OverviewCard.tsx
@@ -2,7 +2,6 @@ import { DateUtils } from '@/lib/utils';
 import { RangeSlider, Stack, Modal, TextInput, Button } from '@mantine/core';
 import { useState } from 'react';
 import Analytics from '../overview/analytics/Analytics';
-import PR from '../overview/pr/PR';
 import TutorialPopover from '../tutorial/TutorialPopover';
 import { ProfileGetter, Team } from '../views/Overview';
 import { TeamData } from '@shared/types/TeamData';
@@ -92,15 +91,6 @@ export const OverviewCard: React.FC<OverviewProps> = ({
           teamDatas={teamDatas}
           selectedWeekRange={selectedWeekRange}
           dateUtils={dateUtils}
-        />
-      </TutorialPopover>
-      <TutorialPopover stage={9} position="top" disabled={index !== 0}>
-        <PR
-          team={team}
-          teamData={teamData}
-          selectedWeekRange={selectedWeekRange}
-          dateUtils={dateUtils}
-          profileGetter={profileGetter}
         />
       </TutorialPopover>
 
