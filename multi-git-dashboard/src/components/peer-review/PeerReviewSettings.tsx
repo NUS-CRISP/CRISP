17dac4ed5fb73228f4a2a2a37ad26fa2090526d1
Gallen <90502827+gongg21@users.noreply.github.com>
2025-11-17 01:12:07 +0800

    run prettier

--- multi-git-dashboard/src/components/peer-review/PeerReviewSettings.tsx
+++ multi-git-dashboard/src/components/peer-review/PeerReviewSettings.tsx
@@ -77,9 +77,11 @@ const PeerReviewSettings: React.FC<PeerReviewSettingsProps> = ({
             {status}
           </Badge>
           <Badge variant="outline">Reviewer Type: {reviewerType}</Badge>
-          <Badge variant="light" color={TaAssignments ? 'teal' : 'red'}>
-            TA Reviews: {TaAssignments ? 'Enabled' : 'Disabled'}
-          </Badge>
+          {hasFacultyPermission && (
+            <Badge variant="light" color={TaAssignments ? 'teal' : 'red'}>
+              TA Reviews: {TaAssignments ? 'Enabled' : 'Disabled'}
+            </Badge>
+          )}
         </Group>
       </Group>
 
@@ -101,19 +103,22 @@ const PeerReviewSettings: React.FC<PeerReviewSettingsProps> = ({
             <Text fz="sm">{formatDate(startDate)}</Text>
           </Stack>
 
-          <Stack gap={2}>
-            <Text fz="xs" c="dimmed">
-              Min. Reviews / Reviewer
-            </Text>
-            <Text fz="sm">{minReviewsPerReviewer ?? '—'}</Text>
-          </Stack>
-
-          <Stack gap={2}>
-            <Text fz="xs" c="dimmed">
-              Team set
-            </Text>
-            <Text fz="sm">{teamSetName}</Text>
-          </Stack>
+          {hasFacultyPermission && (
+            <>
+              <Stack gap={2}>
+                <Text fz="xs" c="dimmed">
+                  Min. Reviews / Reviewer
+                </Text>
+                <Text fz="sm">{minReviewsPerReviewer ?? '—'}</Text>
+              </Stack>
+              <Stack gap={2}>
+                <Text fz="xs" c="dimmed">
+                  Team set
+                </Text>
+                <Text fz="sm">{teamSetName}</Text>
+              </Stack>
+            </>
+          )}
         </Stack>
         <Stack>
           <Stack gap={2}>
@@ -122,40 +127,43 @@ const PeerReviewSettings: React.FC<PeerReviewSettingsProps> = ({
             </Text>
             <Text fz="sm">{formatDate(endDate)}</Text>
           </Stack>
-
-          <Stack gap={2}>
-            <Text fz="xs" c="dimmed">
-              Max. Reviews / Reviewer
-            </Text>
-            <Text fz="sm">{maxReviewsPerReviewer ?? '—'}</Text>
+          {hasFacultyPermission && (
+            <Stack gap={2}>
+              <Text fz="xs" c="dimmed">
+                Max. Reviews / Reviewer
+              </Text>
+              <Text fz="sm">{maxReviewsPerReviewer ?? '—'}</Text>
+            </Stack>
+          )}
+        </Stack>
+        {hasFacultyPermission && (
+          <Stack mt="sm">
+            <Button
+              onClick={onClickUpdate}
+              color="green"
+              variant="light"
+              disabled={status === 'Completed'}
+            >
+              Update Settings
+            </Button>
+            <Button
+              color="red"
+              variant="light"
+              onClick={onClickDelete}
+              disabled={status === 'Completed'}
+            >
+              Delete Peer Review
+            </Button>
+            <Button
+              color="yellow"
+              variant="light"
+              onClick={onClickAssign}
+              disabled={status === 'Completed'}
+            >
+              Assign All Peer Reviews
+            </Button>
           </Stack>
-        </Stack>
-        <Stack mt="sm">
-          <Button
-            onClick={onClickUpdate}
-            color="green"
-            variant="light"
-            disabled={status === 'Completed'}
-          >
-            Update Settings
-          </Button>
-          <Button
-            color="red"
-            variant="light"
-            onClick={onClickDelete}
-            disabled={status === 'Completed'}
-          >
-            Delete Peer Review
-          </Button>
-          <Button
-            color="yellow"
-            variant="light"
-            onClick={onClickAssign}
-            disabled={status === 'Completed'}
-          >
-            Assign All Peer Reviews
-          </Button>
-        </Stack>
+        )}
       </SimpleGrid>
     </Card>
   );
