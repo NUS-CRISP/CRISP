08f055197a329dec3ddd2f060ce991fed2a41275
jyztintan <justintanwk2001@gmail.com>
2025-11-20 16:51:55 +0800

    revert: crisp role renaming

--- shared/types/auth/CrispRole.ts
+++ shared/types/auth/CrispRole.ts
@@ -1,8 +1,10 @@
+// The inconsistent naming format is irritating but we will need to run migrations
+// to fix the naming format. 
 export const CRISP_ROLE = {
   Admin: "admin",
-  TrialUser: "trial_user",
-  Faculty: "faculty",
-  Normal: "normal",
+  TrialUser: "Trial User",
+  Faculty: "Faculty",
+  Normal: "Normal",
 } as const;
 
 export type CrispRole = (typeof CRISP_ROLE)[keyof typeof CRISP_ROLE];
