0e5ce43019e4d8ea6df7b6c23f48d86833f8e544
jyztintan <justintanwk2001@gmail.com>
2026-03-15 22:58:51 +0800

    refactor: replace footer content with CrispLogo component

--- multi-git-dashboard/src/components/home/Footer.tsx
+++ multi-git-dashboard/src/components/home/Footer.tsx
@@ -1,19 +1,14 @@
 import { Anchor, Container, Group } from '@mantine/core';
 import { IconGitBranch } from '@tabler/icons-react';
 import classes from '@styles/Footer.module.css';
-import { Text } from '@mantine/core';
 import Link from 'next/link';
+import CrispLogo from '../shared/CrispLogo';
 
 const Footer: React.FC = () => {
   return (
     <div className={classes.footer}>
       <Container className={classes.inner}>
-        <Group>
-          <IconGitBranch size={50} className={classes.headerIcon} />
-          <Text className={classes.link} style={{ color: 'white' }}>
-            CRISP
-          </Text>
-        </Group>
+        <CrispLogo />
         <Group gap="lg">
           <Anchor component={Link} href="/user-guide" c="gray.4" size="sm">
             User Guide
